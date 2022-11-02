15df1291d860a405d0f3b1e14595681ab6432724
jed212 <jediddahfavour@gmail.com>
2022-11-02 12:52:18 +0300

    I made some more changes to my file

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
 # Dockerfile. image. Container
 FROM python:3.10.6
+WORKDIR /new_csv_file
 
-ADD ona_data_ingestion.py .
-ADD compose1.env .
+COPY ona_data_ingestion.py .
+COPY compose1.env .
 
 RUN pip install requests
 RUN pip install python-dotenv
-RUN pip install csv
 
 CMD [ "python", "./ona_data_ingestion.py"]
--- ona_data_ingestion.py
+++ ona_data_ingestion.py
@@ -1,5 +1,5 @@
 import os
-import requests 
+import requests
 import csv
 
 from dotenv import load_dotenv
@@ -9,29 +9,34 @@ load_dotenv(dotenv_path=f"compose1.env")
 URL = os.getenv("URL")
 USERNAME = os.getenv("USERNAME")
 PASSWORD = os.getenv("PASSWORD")
+
 def get_data_from_ona():
-    auth = (USERNAME, PASSWORD)
-    response =requests.get(url=URL,auth=auth)
+    response=requests.get(url=URL,auth=(USERNAME, PASSWORD))
     
     if response.status_code==200:
         ona_data= response.json()
+        
 
-        with open("ona_data.csv","w", encoding="utf-8") as f:
+        with open("new_csv_file","w", encoding="utf-8") as f:
             writer= csv.writer(f)
-
-            count = 0
+            count=0
             for cnt in ona_data:
-                if count == 0:
+                
+                if count== 0:
                     header =cnt.keys()
                     writer.writerow(header)
-
-                    count >= 1
                     writer.writerow(cnt.values())
 
-            return f
+                    count=count+1
+
+                    
+                elif count>=1:
+                    writer.writerow(cnt.values())
+            return 
     else:
         print(response.status_code)
         print(response.text)
+get_data_from_ona()
+print("Success")
 
-p = get_data_from_ona()
-print(p)
+
