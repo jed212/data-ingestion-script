bcc925f49ff0cb113f1a6de9c88b2f3511fc70be
jed212 <jediddahfavour@gmail.com>
2022-11-09 12:21:18 +0300

    Refactored script_to_db.py

--- script_to_db.py
+++ script_to_db.py
@@ -1,41 +1,70 @@
+
 import psycopg2
 import os
 import requests
 from dotenv import load_dotenv
+
+
 load_dotenv(dotenv_path=f"compose1.env")
 
 USER=os.getenv("USER")
 PASSWORD=os.getenv("PASSWORD")
 HOST=os.getenv("HOST")
 PORT=os.getenv("PORT")
-DATABASE=os.getenv("DATABASE")  
+DATABASE=os.getenv("DATABASE")
 URL = os.getenv("URL")
 ONA_USERNAME = os.getenv("ONA_USERNAME")
 ONA_PASSWORD = os.getenv("ONA_PASSWORD")
 ona_auth=(ONA_USERNAME,ONA_PASSWORD)
 
-
 def fetch_data():
     response=requests.get(url=URL,auth=ona_auth)
-    ona_database_file=response.json()
-    return ona_database_file
- 
+    return response.json()
+  
 
 def create_table():
     commands=('CREATE TABLE IF NOT EXISTS test_2(_id int primary key NOT NULL,_submitted_by varchar NOT NULL,specific_time time);')
-    conn=None
+    execute_statements(commands)
+    return 
+
+
+def insert_statements():
+    table_values=evaluate_json_values()
+    tupl_lst= [tuple (x) for x in table_values]
+    for x in tupl_lst:
+        insert_command=f'insert into test_2(_id,_submitted_by,specific_time) values(%s,%s,%s);'
+        execute_statements(insert_command,*x)
+    return 
+
+
+def connection_to_db(): 
     try:
         connection=psycopg2.connect(user=USER,password=PASSWORD,host=HOST,port=PORT,database=DATABASE)
-        conn=connection.cursor()
-        
-        if connection:
-            print('success')
-        conn.execute(commands)
-        connection.commit()
-        connection.close()
     except Exception as error:
         print(error)
-  
+    return connection
+
+
+def execute_statements(sql_command,*placeholder_values):
+    try:
+        connection=connection_to_db()
+        conn=connection.cursor()
+        if placeholder_values is None:
+            conn.execute(sql_command)
+            connection.commit()
+        if placeholder_values is not None:
+            conn.execute(sql_command,placeholder_values)
+            connection.commit()
+        close_connection(connection)
+    except Exception as error:
+        print(error)
+    return
+
+
+def close_connection(connection):
+    if connection is not None:
+        connection.close()
+        
 
 def evaluate_json_values():
     a=fetch_data()
@@ -44,31 +73,14 @@ def evaluate_json_values():
         first_value=entry["_id"]
         second_value=entry["_submitted_by"]
         third_value=entry.get("specific_time")
+        
         values_to_insert=[(first_value),(second_value),(third_value)]
+        
         var_list.append(values_to_insert)
 
     return var_list
-
-
-def insert_statement(table_values):
-    tupl_lst= [tuple (x) for x in table_values]
-    conn=None
-    try:
-        connection=psycopg2.connect(user=USER,password=PASSWORD,host=HOST,port=PORT,database=DATABASE)
-        conn=connection.cursor()
-        if connection:
-            print('success')
-        for x in tupl_lst:
-           insert_command=f'insert into test_2(_id,_submitted_by,specific_time) values(%s,%s,%s)'
-           conn.execute(insert_command,x)
-        connection.commit()
-        connection.close()
-    except Exception as error:
-        print(error)
-    print(insert_command)
     
 
 if __name__ == '__main__':
     create_table()
-    table_values=evaluate_json_values()
-    insert_statement(table_values)
+    insert_statements()
