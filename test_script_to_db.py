06d5b212429b2a34e12b09d172c2c5e07a94c8f2
jed212 <jediddahfavour@gmail.com>
2022-11-14 17:37:35 +0300

    Test

--- test_script_to_db.py
+++ test_script_to_db.py
@@ -1,7 +1,5 @@
 '''import modules'''
-import psycopg2
-from script_to_db import connection_to_db, evaluate_json_values
-from script_to_db import fetch_data, USER, PASSWORD, HOST, PORT, DATABASE
+from script_to_db import evaluate_json_values, fetch_data
 
 
 def test_evaluate_json_values():
@@ -26,24 +24,11 @@ def test_evaluate_json_values():
     my_list = evaluate_json_values(data_to_use)
     assert len(my_list) == 3
     assert list(zip(*my_list))[0] == (2334, 1113, 2379)
-    # assert my_list[0]
 
-
-def test_connection_to_db():
-    '''testing connection to the database'''
-    connection = psycopg2.connect(
-        user=USER, password=PASSWORD,
-        host=HOST, port=PORT,
-        database=DATABASE)
-    connection_to_db()
-    assert connection is not None
-
-
+    
 def test_fetch_data():
     '''testing data fetched'''
     url = "my_url"
     ona_auth = ("username", "password")
     dict_list = fetch_data(url, ona_auth)
-    assert ("_id" in entry for entry in dict_list)
-    assert ("_submitted_by" in entry for entry in dict_list)
-    assert ("specific_time" in entry for entry in dict_list)
+    assert isinstance(dict_list, list)
